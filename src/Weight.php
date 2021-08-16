7896a69599f1687ddca97e4c74c9d7d803842b7c
tennessine <tennessine@users.noreply.github.com>
2021-08-16 16:17:16 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -2,18 +2,22 @@
 
 namespace Spatie\UnitConversions;
 
-class Weight {
+class Weight
+{
     private float $kilograms;
 
-    public static function fromKilograms( float $kilograms ): self {
-        return new static( $kilograms );
+    public static function fromKilograms(float $kilograms): self
+    {
+        return new static($kilograms);
     }
 
-    public function __construct( float $kilograms ) {
+    public function __construct(float $kilograms)
+    {
         $this->kilograms = $kilograms;
     }
 
-    public function toLbs(): float {
+    public function toLbs(): float
+    {
         return $this->kilograms * 2.204623;
     }
 }
