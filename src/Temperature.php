5e05d9f11e02955350978734f143900674470c12
gekongfei <gekongfei@192.168.2.2>
2021-08-17 01:59:13 +0800

    wip

---
+++ src/Temperature.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Spatie\UnitConversions;
+
+
+class Temperature {
+    private float $celsius;
+
+    public static function forCelsius( float $celsius ) {
+        return new static( $celsius );
+    }
+
+    public function __construct( float $celsius ) {
+        $this->celsius = $celsius;
+    }
+
+    public function toFahrenheit(): float {
+        return ( $this->celsius * 1.8 ) + 32;
+    }
+}
