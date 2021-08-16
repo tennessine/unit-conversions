76dbebd3cd90bb35501f2207412f66784ffdbbc6
tennessine <tennessine@users.noreply.github.com>
2021-08-16 18:03:21 +0000

    Fix styling

--- src/Temperature.php
+++ src/Temperature.php
@@ -1,21 +1,23 @@
 <?php
 
-
 namespace Spatie\UnitConversions;
 
-
-class Temperature {
+class Temperature
+{
     private float $celsius;
 
-    public static function forCelsius( float $celsius ) {
-        return new static( $celsius );
+    public static function forCelsius(float $celsius)
+    {
+        return new static($celsius);
     }
 
-    public function __construct( float $celsius ) {
+    public function __construct(float $celsius)
+    {
         $this->celsius = $celsius;
     }
 
-    public function toFahrenheit(): float {
-        return ( $this->celsius * 1.8 ) + 32;
+    public function toFahrenheit(): float
+    {
+        return ($this->celsius * 1.8) + 32;
     }
 }
--- tests/TemperatureTest.php
+++ tests/TemperatureTest.php
@@ -5,10 +5,12 @@ namespace Spatie\UnitConversions\Tests;
 use PHPUnit\Framework\TestCase;
 use Spatie\UnitConversions\Temperature;
 
-class TemperatureTest extends TestCase {
+class TemperatureTest extends TestCase
+{
     /** @test */
-    public function it_can_convert_celsius_to_fahrenheit() {
-        $fahrenheit = Temperature::forCelsius( 100 )->toFahrenheit();
-        $this->assertEquals( 212, $fahrenheit );
+    public function it_can_convert_celsius_to_fahrenheit()
+    {
+        $fahrenheit = Temperature::forCelsius(100)->toFahrenheit();
+        $this->assertEquals(212, $fahrenheit);
     }
 }
