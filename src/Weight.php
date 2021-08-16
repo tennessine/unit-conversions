6ef01f3135029a066e4d54f51d8dff5e4bd7b191
gekongfei <gekongfei@192.168.2.2>
2021-08-17 01:27:39 +0800

    wip

--- src/Weight.php
+++ src/Weight.php
@@ -2,35 +2,19 @@
 
 namespace Spatie\UnitConversions;
 
-class Weight
-{
-<<<<<<< HEAD
+class Weight {
     /** @var float */
-=======
->>>>>>> 7896a69599f1687ddca97e4c74c9d7d803842b7c
     private float $kilograms;
 
-    public static function fromKilograms(float $kilograms): self
-    {
-<<<<<<< HEAD
-        $myArray = [
-            'test',
-            'test2',
-        ];
-
-
-=======
->>>>>>> 7896a69599f1687ddca97e4c74c9d7d803842b7c
-        return new static($kilograms);
+    public static function fromKilograms( float $kilograms ): self {
+        return new static( $kilograms );
     }
 
-    public function __construct(float $kilograms)
-    {
+    public function __construct( float $kilograms ) {
         $this->kilograms = $kilograms;
     }
 
-    public function toLbs(): float
-    {
+    public function toLbs(): float {
         return $this->kilograms * 2.204623;
     }
 }
